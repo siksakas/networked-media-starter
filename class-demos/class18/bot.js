76694c6ac404c32245716d4e3ca9695da1907755
Siksaka Suriyasat <113951267+siksakas@users.noreply.github.com>
2026-03-31 17:09:20 -0400

    sfvs

---
+++ class-demos/class18/bot.js
@@ -0,0 +1,29 @@
+// imports the .env library and allows us to access the variables in the .env file by using process.env.VARIABLE_NAME
+require('dotenv').config();
+
+const m = require('masto');
+
+// set up ability to use masto library
+// similar to making app
+
+const masto = m.createRestAPIClient({
+    url: 'https://networked-media.itp.io',
+    accessToken: process.env.TOKEN
+})
+
+const makeStatus = async () => {
+    // customize bla bla bla bla bla bla bla bla 
+    let emojis = ['😀', '😂', '😍', '🤔', '🙄', '😎', '😭', '😡', '👍', '👎'];
+
+    let randomSelection = Math.floor(Math.random() * emojis.length);
+    
+    const s = await masto.v1.statuses.create({
+        status: emojis[randomSelection],
+        visibility: "public"
+    });
+    console.log(s.url);
+}
+
+//makeStatus();
+//post a status every 10 seconds
+setInterval(makeStatus,10000);
